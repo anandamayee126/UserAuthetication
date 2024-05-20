83dda2f92559d935213a35364ca1afb38187a376
Anandamayee Ghosh <65214830+anandamayee126@users.noreply.github.com>
2024-05-20 17:45:22 +0530

    login and signup page added

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app= express();
+import cors from 'cors';
+import User from './routes/user';
+import mongoose from 'mongoose';
+let mongoConnect= await mongoose.connect('mongodb+srv://ghoshanandamayee:LL4W9mIpbWEZHeok@cluster0.mfihvdl.mongodb.net/');
+console.log("connect",mongoConnect);
+
+app.use(cors());
+app.use(express.json());
+app.use('/user',User);
---
+++ frontend/login.js
@@ -0,0 +1,17 @@
+const login= document.getElementById('login')
+login.addEventListener('submit', checkUser);
+
+
+async function checkUser(e){
+    e.preventDefault()
+    const name= e.target.name.value;
+    const email= e.target.email.value;
+    const password= e.target.password.value;
+
+    const user={
+        name,email,password
+    }
+    
+    const login= await axios.post('http://localhost:4000/user/login',user);
+    console.log("login",login);
+}
